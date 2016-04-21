b5912c37d6a00a6985d94bbaadaa975b225f3a42
Mikhail Gashenko <dr.psycho@live.com>
2016-04-21 22:24:24 +0600

    finish try 2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
 import App from './containers/App'
-import configureStore from './store/configureStore'
 import './styles/app.css'
+import configureStore from './store/configureStore'
 
 const store = configureStore()
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,16 +3,21 @@ import rootReducer from '../reducers'
 import createLogger from 'redux-logger'
 import thunk from 'redux-thunk'
 
+
 export default function configureStore(initialState) {
-  const logger = createLogger()
-  const store = createStore(rootReducer, initialState, applyMiddleware(thunk, logger))
+    const logger = createLogger()
+    const store = createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(thunk, logger))
 
-  if (module.hot) {
-    module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers')
-      store.replaceReducer(nextRootReducer)
-    })
-  }
+    if (module.hot) {
+        module.hot.accept('../reducers', () => {
+            const nextRootReducer = require('../reducers')
+            store.replaceReducer(nextRootReducer)
+        })
+    }
 
-  return store
+    return store
 }
+
